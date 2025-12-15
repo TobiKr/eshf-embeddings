61ce436481bef52ecaea1a3c7523724c130f4c01
TobiKr <mail@tobiaskritten.de>
2025-12-15 10:53:20 +0100

    Update postDiscovery timer schedule to run every 5 minutes

--- src/postDiscovery.ts
+++ src/postDiscovery.ts
@@ -128,8 +128,8 @@ async function postDiscoveryHandler(
 }
 
 // Register the timer-triggered function
-// Schedule: '0 */5 * * * *' = every 10 minutes
+// Schedule: '0 */5 * * * *' = every 5 minutes
 app.timer('postDiscovery', {
-  schedule: '0 */10 * * * *',
+  schedule: '0 */5 * * * *',
   handler: postDiscoveryHandler,
 });
